92f2bddc0a5bb452de9d1e6ec66c7a8085f1fb86
mikaelfr <mikael.fredriksson@helsinki.fi>
2019-12-23 01:49:23 +0200

    Add admin login page and login functionality.

--- src/main/java/sec/project/config/CustomUserDetailsService.java
+++ src/main/java/sec/project/config/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package sec.project.config;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import javax.annotation.PostConstruct;
@@ -19,7 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public void init() {
         // this data would typically be retrieved from a database
         this.accountDetails = new TreeMap<>();
-        this.accountDetails.put("ted", "$2a$06$rtacOjuBuSlhnqMO2GKxW.Bs8J6KI0kYjw/gtF0bfErYgFyNTZRDm");
+        this.accountDetails.put("admin", "$2a$04$XgDznnMZPL/a429JaeiqH.kt/5YORfSB3VBjz138kZCfnmkpUQPFK");
     }
 
     @Override
@@ -28,6 +30,13 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("No such user: " + username);
         }
 
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        
+        if (username.equals("admin")) {
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        }
+        
         return new org.springframework.security.core.userdetails.User(
                 username,
                 this.accountDetails.get(username),
@@ -35,6 +44,6 @@ public class CustomUserDetailsService implements UserDetailsService {
                 true,
                 true,
                 true,
-                Arrays.asList(new SimpleGrantedAuthority("USER")));
+                authorities);
     }
 }
--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -3,6 +3,12 @@ package sec.project.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +22,9 @@ public class SignupController {
 
     @Autowired
     private SignupRepository signupRepository;
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @RequestMapping("*")
     public String defaultMapping() {
@@ -35,7 +44,30 @@ public class SignupController {
 
     @RequestMapping(value = "/signed", method = RequestMethod.GET)
     public String loadSigned(Model model) {
-        model.addAttribute("list", signupRepository.findAll().stream().map(s -> s.getName()).collect(Collectors.toList()));
+        List<String> mapped = signupRepository.findAll().stream().map(s -> s.getName()).collect(Collectors.toList());
+        model.addAttribute("list", mapped);
         return "signed";
+    }
+    
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String loadAdmin(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated() && auth.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
+            System.out.println(auth.getCredentials());
+            List<String> mapped = signupRepository.findAll().stream().map(s -> s.getName() + " - " + s.getAddress()).collect(Collectors.toList());
+            model.addAttribute("list", mapped);
+        }
+        
+        return "admin";
+    }
+    
+    @RequestMapping(value = "/admin", method = RequestMethod.POST)
+    public String submitLogin(@RequestParam String username, @RequestParam String password) {
+        if (userDetailsService.loadUserByUsername(username) != null && BCrypt.checkpw(password, userDetailsService.loadUserByUsername(username).getPassword())) {
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, password);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        
+        return "redirect:/admin";
     }
 }
